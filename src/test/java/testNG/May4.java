367eb980a464f9e9291533248798be8b27a971a6
bhel123 <atulkumar1798.ak@gmail.com>
2024-05-04 19:11:58 +0530

    2nd time comment

--- src/test/java/testNG/May4.java
+++ src/test/java/testNG/May4.java
@@ -8,22 +8,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
 
-public class May3 {
+public class May4 {
+	
 	WebDriver driver;
 	
   @Test(dataProvider = "dp")
   public void f(Integer n, String s) {
-	 driver.findElement(By.xpath("//input[@type='text']")).sendKeys("6393993770");
-	 driver.findElement(By.xpath("//input[@value='Continue']")).click();
-	// driver.findElement(By.t)
-	  
-	  
+	  driver.findElement(By.name("username")).sendKeys("atul@gmail.com");
+	  driver.findElement(By.xpath("//input[@name='password']")).sendKeys("1234567890");
+	  driver.findElement(By.xpath("//input[@id='btn_login']")).click();
   }
   @BeforeMethod
   public void beforeMethod() {
 	  driver = new ChromeDriver();
-	  driver.get("https://accounts.flipkart.com/?client=cemp-client&redirectURL=httpsmerchant.ekartlogistics.comcallbackappNamemerchant");
-	 // driver.close();
+	  driver.get("https://www.kamera-express.nl/login");
+	  
+	  
+	  
   }
 
   @AfterMethod
@@ -35,7 +36,7 @@ public class May3 {
   public Object[][] dp() {
     return new Object[][] {
       new Object[] { 1, "a" },
-//      new Object[] { 2, "b" },
+      new Object[] { 2, "b" },
     };
   }
 }
